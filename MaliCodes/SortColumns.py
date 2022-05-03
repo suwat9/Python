12a00a07984b4dcbbeb5753cd142ad3396413cdd
Suwat <suwat.tac@mail.pbru.ac.th>
2022-05-03 14:46:25 +0700

    Add files via upload

---
+++ MaliCodes/SortColumns.py
@@ -0,0 +1,19 @@
+import pandas as pd    
+from os import walk
+
+path = 'dataset/DaisoThailand - MAY2022 - 825SKU'
+filenames = next(walk(path), (None, None, []))[2]  # [] if no file
+
+f = filenames[0]
+df = pd.read_excel(path+'\\'+f, sheet_name = 'Data', usecols = 'A:T')
+
+cols = list(df.columns)
+print(cols)
+Cprice, Cimg ,Cdes = cols.index('Price'),cols.index('Image 1'),cols.index('Description')
+
+cols[Cprice], cols[Cimg], cols[Cdes] = cols[Cdes], cols[Cprice], cols[Cimg]
+df = df[cols] 
+
+print(list(df.columns))
+
+print(df.head())
