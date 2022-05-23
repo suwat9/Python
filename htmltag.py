9ca8aef8117550259e2e92b70fe4983f9008d748
Suwat <suwat.tac@mail.pbru.ac.th>
2022-05-23 16:08:01 +0700

    Create htmltag.py

---
+++ htmltag.py
@@ -0,0 +1,7 @@
+from htmltag import a,span,div,img
+
+print(span('My data', style='font-family:none'))
+print(div(div(div(img(src="http://it.pbru.ac.th",style="width:100%;display:block"),
+        style="width:100%;display:block"),
+        style="width:100%;display:block;margin:0"),
+        style="width:100%;margin:0"))
